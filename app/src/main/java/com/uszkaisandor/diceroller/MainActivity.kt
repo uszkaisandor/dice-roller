3d131766413a0a6da78f6aa8bd0d27f6d843087a
Sándor Uszkai <uszkaisanyi@gmail.com>
2020-07-22 20:12:16 +0200

    Implement dice rolling on button click

--- app/src/main/java/com/uszkaisandor/diceroller/MainActivity.kt
+++ app/src/main/java/com/uszkaisandor/diceroller/MainActivity.kt
@@ -2,11 +2,29 @@ package com.uszkaisandor.diceroller
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        rollButton?.text = applicationContext.getString(R.string.lets_roll)
+        rollButton?.setOnClickListener {
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val drawableResource = when (Random.nextInt(6) + 1) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
     }
 }
